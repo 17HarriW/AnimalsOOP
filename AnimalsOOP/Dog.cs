6a62c08bd31b89087d7b0914996c5e1ad7a5af3f
William Harris <17HarriW@fulford.york.sch.uk>
2022-10-10 14:48:44 +0100

    Add project files.

---
+++ AnimalsOOP/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsOOP
+{
+    class Dog
+    {
+        public void Fetch()
+        {
+
+        }
+
+        public void Bark()
+        {
+
+        }
+
+        public void Roll()
+        {
+
+        }
+    }
+}
---
+++ AnimalsOOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog John = new Dog();
+            Hedgehog Brian = new Hedgehog();
+
+            John.Fetch();
+            John.Eat();
+            John.Roll();
+            John.Bark();
+
+            Brian.Squeak();
+            Brian.Eat();
+        }
+    }
+}
